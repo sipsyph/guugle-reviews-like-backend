b589dfef7c858eb17f346cea594ffe5f73e106a4
Jance <jancep@sxi.com.ph>
2022-06-07 22:15:48 +0800

    Added some memoir fields as params for searching places

--- src/main/java/com/sip/chillhub/main/business/model/PlaceSearchRequest.java
+++ src/main/java/com/sip/chillhub/main/business/model/PlaceSearchRequest.java
@@ -9,7 +9,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties
 public class PlaceSearchRequest {
-	
+
+
+
 	public List<Long> getId() {
 		return id;
 	}
@@ -30,6 +32,64 @@ public class PlaceSearchRequest {
 	}
 
 
+	public List<Integer> getCategoryType() {
+		return categoryType;
+	}
+
+
+	public void setCategoryType(List<Integer> categoryType) {
+		this.categoryType = categoryType;
+	}
+
+
+	public List<Integer> getDescType() {
+		return descType;
+	}
+
+
+	public void setDescType(List<Integer> descType) {
+		this.descType = descType;
+	}
+
+
+	public List<Integer> getPeopleTrafficType() {
+		return peopleTrafficType;
+	}
+
+
+	public void setPeopleTrafficType(List<Integer> peopleTrafficType) {
+		this.peopleTrafficType = peopleTrafficType;
+	}
+
+
+	public Double getCoordinatesX() {
+		return coordinatesX;
+	}
+
+
+	public void setCoordinatesX(Double coordinatesX) {
+		this.coordinatesX = coordinatesX;
+	}
+
+
+	public Double getCoordinatesY() {
+		return coordinatesY;
+	}
+
+
+	public void setCoordinatesY(Double coordinatesY) {
+		this.coordinatesY = coordinatesY;
+	}
+
+	public String getSortByUpvotes() {
+		return sortByUpvotes;
+	}
+
+
+	public void setSortByUpvotes(String sortByUpvotes) {
+		this.sortByUpvotes = sortByUpvotes;
+	}
+
 	public List<String> getOrderByAndSortBy() {
 		return orderByAndSortBy;
 	}
@@ -38,35 +98,63 @@ public class PlaceSearchRequest {
 	public void setOrderByAndSortBy(List<String> orderByAndSortBy) {
 		this.orderByAndSortBy = orderByAndSortBy;
 	}
-	
+
+
 	public boolean isObj() {
 		return obj;
 	}
 
+
 	public void setObj(boolean obj) {
 		this.obj = obj;
 	}
-
-
-	@Override
-	public String toString() {
-		return "PlaceSearchRequest [id=" + id + ", searchString=" + searchString + ", orderByAndSortBy="
-				+ orderByAndSortBy + "]";
-	}
-
-
+	
 	private List<Long> id;
 	private String searchString;
+	private List<Integer> categoryType;
+	private List<Integer> descType;
+	private List<Integer> peopleTrafficType;
+	private Double coordinatesX;
+	private Double coordinatesY;
+	private String sortByUpvotes;
 	private List<String> orderByAndSortBy;
 	private boolean obj;
 
 	
 	public boolean isEmpty() {
-		if((id==null||id.isEmpty())&&searchString==null) {
+		if(
+				(id==null||id.isEmpty())&&
+				searchString==null&&
+				coordinatesX==null&&
+				coordinatesY==null) {
 			return true;
 		}
 		return false;
 	}
+	
+	public boolean isMemoirAttributesEmpty() {
+		if(
+				(categoryType==null||categoryType.isEmpty())&&
+				(descType==null||descType.isEmpty())&&
+				(peopleTrafficType==null||peopleTrafficType.isEmpty())&&
+				sortByUpvotes==null) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isMemoirAttributesEmptyExceptUpvotesSort() {
+		if(
+				(categoryType==null||categoryType.isEmpty())&&
+				(descType==null||descType.isEmpty())&&
+				(peopleTrafficType==null||peopleTrafficType.isEmpty())&&
+				sortByUpvotes!=null) {
+			return true;
+		}
+		return false;
+	}
+
+
 
 
 
--- src/main/java/com/sip/chillhub/main/business/service/PlaceReadServiceImpl.java
+++ src/main/java/com/sip/chillhub/main/business/service/PlaceReadServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sip.chillhub.main.business.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,45 +26,103 @@ public class PlaceReadServiceImpl implements PlaceReadService {
 		final StringBuilder sqlStatement = new StringBuilder(500);
 		MapSqlParameterSource sqlParams = new MapSqlParameterSource();
 		if(request.isObj()) {
-			sqlStatementUntilWherePredicate.append("SELECT * FROM place ");
+			sqlStatementUntilWherePredicate.append("SELECT * FROM place p ");
 		}else {
-			sqlStatementUntilWherePredicate.append("SELECT id FROM place ");
+			sqlStatementUntilWherePredicate.append("SELECT id FROM place p ");
 		}
+		
+		List<String> orderByAndSortByRequest = new ArrayList<>();
+		
+		if(request.getOrderByAndSortBy()!=null) {
+			orderByAndSortByRequest.addAll(request.getOrderByAndSortBy());
+		}
+		
+		if(!request.isMemoirAttributesEmpty()) {
+			
+			sqlStatementUntilWherePredicate.append("INNER JOIN LATERAL ( ");
+			sqlStatementUntilWherePredicate.append("SELECT SUM(m.ups) AS upvotes FROM memoir m ");
+			
+			if(!request.isMemoirAttributesEmptyExceptUpvotesSort()) {
+				sqlStatementUntilWherePredicate.append("WHERE");
+			}
+			
+			if(request.getCategoryType()!=null && !request.getCategoryType().isEmpty()) {
+				sqlStatementUntilWherePredicate.append("m.category_type IN (:categoryType) AND ");
+				sqlParams.addValue("categoryType", request.getId());
+			}
+			
+			if(request.getDescType()!=null && !request.getDescType().isEmpty()) {
+				sqlStatementUntilWherePredicate.append("m.desc_type IN (:descType) AND ");
+				sqlParams.addValue("descType", request.getDescType());
+			}
+			
+			if(request.getPeopleTrafficType()!=null && !request.getPeopleTrafficType().isEmpty()) {
+				sqlStatementUntilWherePredicate.append("m.people_traffic_type IN (:peopleTrafficType) AND ");
+				sqlParams.addValue("peopleTrafficType", request.getPeopleTrafficType());
+			}
+			
+			sqlStatementUntilWherePredicate.append("GROUP BY m.place_id ) AS memoir ON true ");
+			
+		}
+			
 		if(!request.isEmpty()) {
+			
 			sqlStatementUntilWherePredicate.append("WHERE ");
 			
 			if(request.getId()!=null && !request.getId().isEmpty()) {
-				sqlStatementUntilWherePredicate.append("id IN (:id) AND ");
+				sqlStatementUntilWherePredicate.append("p.id IN (:id) AND ");
 				sqlParams.addValue("id", request.getId());
 			}
 			
 			if(request.getSearchString()!=null) {
-				sqlStatementUntilWherePredicate.append("name LIKE :searchString AND ");
+				sqlStatementUntilWherePredicate.append("p.name LIKE :searchString AND ");
 				sqlParams.addValue("searchString", request.getSearchString());
+			}
+			
+			if(request.getSortByUpvotes()!=null) {
+				System.out.println(request.getSortByUpvotes());
+				orderByAndSortByRequest.add("memoir.upvotes "+request.getSortByUpvotes());
+			}
+			
+			if(request.getCoordinatesX()!=null && request.getCoordinatesY()!=null) {
+				orderByAndSortByRequest.add("abs(p.coordinates_x-:userCoordinatesX) + abs(p.coordinates_y-:userCoordinatesY) ASC");
+				sqlParams.addValue("userCoordinatesX", request.getCoordinatesX());
+				sqlParams.addValue("userCoordinatesY", request.getCoordinatesY());
 			}
 			
 			if(request.isObj()) {
 				return (List<T>) placeRepository.findMemoirBySearchRequestParameters(
-						SQLGenericStatementBuilder.orderBy(request.getOrderByAndSortBy(), 
+						SQLGenericStatementBuilder.orderBy(orderByAndSortByRequest, 
 								SQLGenericStatementBuilder.removeLastOccurrenceOfAndKeyword(sqlStatementUntilWherePredicate)).toString(), 
 						sqlParams);
 			}
 			
 			return (List<T>) placeRepository.findIdBySearchRequestParameters(
-					SQLGenericStatementBuilder.orderBy(request.getOrderByAndSortBy(), 
+					SQLGenericStatementBuilder.orderBy(orderByAndSortByRequest, 
 							SQLGenericStatementBuilder.removeLastOccurrenceOfAndKeyword(sqlStatementUntilWherePredicate)).toString(), 
 					sqlParams);
 			
 		}else {
 			sqlStatement.append(sqlStatementUntilWherePredicate);
 			
+			if(request.getSortByUpvotes()!=null) {
+				System.out.println(request.getSortByUpvotes());
+				orderByAndSortByRequest.add("memoir.upvotes "+request.getSortByUpvotes());
+			}
+			
+			if(request.getCoordinatesX()!=null && request.getCoordinatesY()!=null) {
+				orderByAndSortByRequest.add("abs(p.coordinates_x-:userCoordinatesX) + abs(p.coordinates_y-:userCoordinatesY) ASC");
+				sqlParams.addValue("userCoordinatesX", request.getCoordinatesX());
+				sqlParams.addValue("userCoordinatesY", request.getCoordinatesY());
+			}
+			
 			if(request.isObj()) {
 				return (List<T>) placeRepository.findMemoirBySearchRequestParameters(
-						SQLGenericStatementBuilder.orderBy(request.getOrderByAndSortBy(), sqlStatement).toString(), 
+						SQLGenericStatementBuilder.orderBy(orderByAndSortByRequest, sqlStatement).toString(), 
 						sqlParams);
 			}
 			return (List<T>) placeRepository.findIdBySearchRequestParameters(
-					SQLGenericStatementBuilder.orderBy(request.getOrderByAndSortBy(), sqlStatement).toString(), 
+					SQLGenericStatementBuilder.orderBy(orderByAndSortByRequest, sqlStatement).toString(), 
 					sqlParams);
 		}
 	}
